8c461d0f1d6c62a076caddcbf6a42be657347fdf
Vamsi Krishna <115712135+Thalam-Vamsi-Krishna@users.noreply.github.com>
2023-03-31 18:33:09 +0530

    successfully implemented

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -42,5 +42,45 @@ const cartSlice=createSlice({
        }
     }
 })
+
+const sendCartData=()=>{
+    return async(dispatch)=>{
+        dispatch(uiActions.showNotification({
+            status:'pending',
+            title:'sending...',
+            message:'sending data to cart'
+          }))
+
+
+       const sendRequest=async()=>{
+        const response=await fetch('https://advanced-redux-d8c56-default-rtdb.firebaseio.com/cart.json',{
+            method:'PUT',
+            body:JSON.stringify(cart)
+          })
+          if(!response.ok){
+            throw new Error('sending cart data failed')
+            
+          }
+       }
+       
+       try{
+            await sendRequest();
+            dispatch(uiActions.showNotification({
+                status:'success',
+                title:'sucesss...',
+                message:'sent data to cart successfully'
+              }))
+        }
+        catch(error){
+            dispatch(uiActions.showNotification({
+                status:'error',
+                title:'Error...',
+                message:'sending data to cart failed'
+              }))
+        }
+       
+     
+    }
+}
 export default cartSlice.reducer;
 export const cartActions=cartSlice.actions;
